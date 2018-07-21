d223ea56e9cd3d58f6499c7045138544a0f202b7
cipepser <respepic@gmail.com>
2018-07-21 11:05:17 +0900

    implement Mutation_postMessage

--- server/graphql.go
+++ server/graphql.go
@@ -6,7 +6,10 @@ import (
 	// 	"fmt"
 	// 	"log"
 
+	"context"
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"sync"
 	"time"
@@ -14,9 +17,9 @@ import (
 	"github.com/go-redis/redis"
 	"github.com/gorilla/websocket"
 	"github.com/rs/cors"
-	"github.com/vektah/gqlgen/handler"
-	// 	"github.com/segmentio/ksuid"
+	"github.com/segmentio/ksuid"
 	"github.com/tinrab/retry"
+	"github.com/vektah/gqlgen/handler"
 )
 
 // type contextKey string
@@ -88,37 +91,32 @@ func (s *graphQLServer) createUser(user string) error {
 	return nil
 }
 
-// func (s *graphQLServer) Mutation_postMessage(ctx context.Context, user string, text string) (*Message, error) {
-// 	err := s.createUser(user)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-//
-// 	// Create message
-// 	m := Message{
-// 		ID:        ksuid.New().String(),
-// 		CreatedAt: time.Now().UTC(),
-// 		Text:      text,
-// 		User:      user,
-// 	}
-// 	mj, _ := json.Marshal(m)
-// 	if err := s.redisClient.LPush("messages", mj).Err(); err != nil {
-// 		log.Println(err)
-// 		return nil, err
-// 	}
-// 	// Notify new message
-// 	s.mutex.Lock()
-// 	for _, ch := range s.messageChannels {
-// 		ch <- m
-// 	}
-// 	s.mutex.Unlock()
-// 	return &m, nil
-// }
-//
+func (s *graphQLServer) Mutation_postMessage(ctx context.Context, user string, text string) (*Message, error) {
+	if err := s.createUser(user); err != nil {
+		return nil, err
+	}
 
-// func (s *graphQLServer) Mutation_postMessage(ctx context.Context, user string, text string) (*Message, error) {
-//
-// }
+	// Create message
+	m := Message{
+		ID:        ksuid.New().String(),
+		User:      user,
+		CreatedAt: time.Now().UTC(),
+		Text:      text,
+	}
+	mj, _ := json.Marshal(m)
+	if err := s.redisClient.LPush("message", mj).Err(); err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	// Notify new massage
+	s.mutex.Lock()
+	for _, ch := range s.messageChannels {
+		ch <- m
+	}
+	s.mutex.Unlock()
+	return &m, nil
+}
 
 /////////////////////////////////////////////////////////////////////////////////////
 // TODO: implement methods for Resolvers
